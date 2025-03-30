1c4b966d614e7fd5640f155057001bf903306884
maxim <bozzhik@ya.ru>
2025-03-30 17:16:04 +0300

    init `HeroFall` sub-block based on `Gravity`

--- src/components/App/index/Hero.tsx
+++ src/components/App/index/Hero.tsx
@@ -1,20 +1,27 @@
 import {cn} from '@/lib/utils'
 
 import {H1, P} from '~/UI/Typography'
+import HeroFall from '~~/index/HeroFall'
 
 export default function Hero() {
-  const SCREEN_HEIGHT = 'h-screen !h-svh' // from-section/0 via-section/30 to-section
+  const SCREEN_HEIGHT = 'h-screen !h-svh'
 
   return (
     <section data-section="hero-index" className={cn('grid place-items-center p-10', SCREEN_HEIGHT)}>
-      <div className={cn('flex flex-col items-center', 'pt-20 size-full rounded-[30px]', 'bg-gradient-to-b from-section/80 to-section')}>
-        <div className="flex flex-col items-center gap-3">
-          <H1 className="text-center" offset={0}>
-            Pu<span className="tracking-[0em]">f</span>ferz Are Falling <br /> Into Your Wallet
-          </H1>
+      <div className={cn('relative flex size-full')}>
+        <div className={cn('relative z-20', 'pt-20 size-full')}>
+          <div className="flex flex-col items-center gap-3">
+            <H1 className="text-center" offset={0}>
+              Pu<span className="tracking-[0em]">f</span>ferz Are Falling <br /> Into Your Wallet
+            </H1>
 
-          <P by="word">Exclusive NFT collection on SUI. Dive in, discover, collect.</P>
+            <P by="word">Exclusive NFT collection on SUI. Dive in, discover, collect.</P>
+          </div>
         </div>
+
+        <div className={cn('absolute inset-0 w-full h-full', 'rounded-[30px] bg-gradient-to-b from-section/80 to-section')}></div>
+
+        <HeroFall />
       </div>
     </section>
   )
