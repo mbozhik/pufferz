fd787a1d8538058a901700b0b2320ce010323917
maxim <bozzhik@ya.ru>
2025-03-30 19:11:22 +0300

    init custom `use-media-query` hook using `shadcn/ui`
    https://github.com/shadcn-ui/ui/blob/1081536246b44b6664f4c99bc3f1b3614e632841/apps/www/hooks/use-media-query.tsx

--- src/components/App/index/HeroGallery.tsx
+++ src/components/App/index/HeroGallery.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import {cn} from '@/lib/utils'
+import {useMediaQuery} from '@/utils/use-media-query'
+
 import CircularGallery from '~/Module/CircularGallery'
 
 export default function HeroGallery({className}: {className?: string}) {
+  const isDesktop = useMediaQuery('(min-width: 768px)')
+
   return (
     <div data-section="hero-gallery" className={cn('relative h-[700px]', className)}>
-      <CircularGallery bend={3} textColor="#7f7f7f" borderRadius={0.05} />
+      <CircularGallery bend={isDesktop ? 3 : 1} textColor="#7f7f7f" borderRadius={0.05} />
     </div>
   )
 }
