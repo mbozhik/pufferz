7cca6a0b8b17f95ef0e49200aeeb7eca847b7950
maxim <bozzhik@gmail.com>
2025-08-16 18:06:29 +0700

    refactor `Container` Global component

--- src/components/Global/Container.tsx
+++ src/components/Global/Container.tsx
@@ -1,8 +1,19 @@
 import {cn} from '@/lib/utils'
 
-export const WEBSITE_BOX = 'px-40 xl:px-32 sm:w-auto sm:px-1.5'
-export const WEBSITE_OFFSET = 'pt-40 xl:pt-36 sm:pt-32'
+export const BOX = {
+  container: 'w-[80%] xl:w-[85%] sm:w-auto mx-auto sm:mx-1.5',
+  content: 'w-[45vw] xl:w-[55vw] sm:w-auto mx-auto sm:mx-1.5',
+}
+
+export const CONTAINER = {
+  paddings: 'py-[35vh] xl:py-[35vh] sm:py-[25vh] sm:pb-[10vh]',
+  spacing: 'space-y-[30vh] xl:space-y-[30vh] sm:space-y-[20vh]',
+}
+
+export const CONTENT = {
+  offset: 'pt-40 xl:pt-36 sm:pt-32',
+}
 
 export default function Container({children, className}: {children: React.ReactNode; className?: string}) {
-  return <main className={cn(WEBSITE_BOX, 'py-[35vh] xl:py-[35vh] sm:py-[25vh] sm:pb-[10vh]', 'space-y-[30vh] xl:space-y-[30vh] sm:space-y-[20vh]', className)}>{children}</main>
+  return <main className={cn(BOX.container, CONTAINER.paddings, CONTAINER.spacing, className)}>{children}</main>
 }
